b4ef88e4d9c9d67028a1899d0fe0f43c3e346b1a
OragantiSagar041 <yourgithubemail@gmail.com>
2026-07-30 18:35:46 +0530

    updated

--- Front-end/src/apiConfig.js
+++ Front-end/src/apiConfig.js
@@ -1,6 +1,6 @@
 const isLocal = ["localhost", "127.0.0.1"].includes(window.location.hostname)
 const configuredBaseUrl = String(import.meta.env.VITE_API_BASE_URL || '').trim().replace(/\/+$/, '')
-const productionDefault = 'https://kr-d21dfc264e90465499a72a823e039dee.ecs.us-east-1.on.aws'
+const productionDefault = 'http://ecs-express-gateway-alb-56fd9253-1587714726.us-east-1.elb.amazonaws.com'
 
 export const API_BASE_URL = configuredBaseUrl || (isLocal ? 'http://127.0.0.1:8000' : productionDefault)
 export const API_BASE = API_BASE_URL
