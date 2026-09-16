e5f7dff9f0925809bf657a301af193420c317ae4
Omkar <ingleomkarpavan@gmail.com>
2026-09-16 12:30:52 +0530

    AWS reverted

--- Front-end/src/apiConfig.js
+++ Front-end/src/apiConfig.js
@@ -5,7 +5,11 @@ const isLocalHostname = (hostname) => {
          hostname.startsWith("172.");
 };
 
+const isLocal = typeof window !== "undefined" && isLocalHostname(window.location.hostname);
+const configuredBaseUrl = (typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.VITE_API_URL) ? import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '') : '';
+
 const LOCAL_URL = 'http://127.0.0.1:8000'
 const PROD_URL = 'http://sb-lb-1304167006.us-east-1.elb.amazonaws.com'
 export const API_BASE_URL = configuredBaseUrl || (isLocal ? LOCAL_URL : PROD_URL)
 export const API_BASE = API_BASE_URL
+
